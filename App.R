a28450d3e48c5a20386f876e475abe18fb4f4e69
JMGilbert <jonahmgilbert1@gmail.com>
2018-12-14 15:05:55 -0500

    Commented code.

--- App.R
+++ App.R
@@ -7,6 +7,12 @@ library(tidyr)
 library(FNN)
 source('config.R')
 
+# check(url)
+# inputs: url to get data from
+# outputs: json data as df
+# 
+# This function's purpose is to circumvent 409 errors (too many requests)
+# Will retry each second for 5 seconds
 check <- function(url){
   attempt <- 0
   res <- NULL
@@ -22,6 +28,7 @@ check <- function(url){
   return(res)
 }
 
+# Gets an access token from the spotify api
 post <- POST('https://accounts.spotify.com/api/token',
              accept_json(), authenticate(client_id, client_secret),
              body = list(grant_type = 'client_credentials'),
@@ -49,7 +56,7 @@ ui <- fluidPage(
     # Main panel for displaying outputs ----
     mainPanel(
       
-      # Output: Tabset w/ plot, summary, and table ----
+      # Output: Tabset w/ plot ----
       tabsetPanel(type = "tabs",
                   tabPanel("Playlist Table", htmlOutput("statement"),DT::dataTableOutput("table")))
       
@@ -60,37 +67,69 @@ ui <- fluidPage(
 # Define server logic for random distribution app ----
 server <- function(input, output) {
   
-  # Reactive expression to generate the requested distribution ----
-  # This is called whenever the inputs change. The output functions
-  # defined below then use the value computed from this expression
-  playlist <- reactive({
+  # Reactive function returning name of playlist, number of tracks, and number of followers
+  playlistdata <- reactive({
+    # Gets an access token from the spotify api, expires every hour so this needs to be done with some regularity
+    post <- POST('https://accounts.spotify.com/api/token',
+                 accept_json(), authenticate(client_id, client_secret),
+                 body = list(grant_type = 'client_credentials'),
+                 encode = 'form', httr::config(http_version = 2)) %>% content
+    access_token <- post$access_token
+    
+    playlist_id <- input$playlist_id
+    print(playlist_id)
+    
+    # Gets playlist information from API
+    url1 <- paste("https://api.spotify.com/v1/playlists/",playlist_id,"?access_token=",access_token,sep = "")
+    playlist <- check(url1)
+    
+    playlistinfo <- NULL
+    playlistinfo$name <- playlist$name
+    playlistinfo$songs <- playlist$tracks$total
+    playlistinfo$followers<- playlist$followers$total
+    playlistinfo
+  })
+  
+    # Reactive function to get tracks in the playlist
+    playlist <- reactive({
     inf <- playlistdata()
     playlist_id <- input$playlist_id
     leng <- inf$songs
     loops <- as.integer(leng/100)
     
+    # Gets all song ids in the playlist
     songs <- NULL
     for(j in 0:loops){
       offset <- j*100
-      url <- paste("https://api.spotify.com/v1/playlists/",playlist_id,"/tracks?access_token=",access_token,"&limit=100&offset=",offset,sep = "")
-      df <- check(url)
+      url2 <- paste("https://api.spotify.com/v1/playlists/",playlist_id,"/tracks?access_token=",access_token,"&limit=100&offset=",offset,sep = "")
+      df <- check(url2)
       songs <- c(songs,df$items$track$id)
-      
+    
+    # Ensures the playlist has tracks  
     }
     if(is.null(songs)){
       return(NULL)
     }
     loops2 <- ceiling(length(songs)/50)
     
+    # Dataframe for song information
     data2 <- data.frame()
+    
+    # Loops through all song ids, and gets information (title, artists, duration, popularity, etc)
     for(k in 1:loops2){
       ids <- songs[((k-1)*50+1):(k*50)]
       ids <- ids[!is.na(ids)]
       ids <- paste(ids,collapse=",")
-      url1 <- paste("https://api.spotify.com/v1/audio-features/?access_token=",access_token,"&ids=",ids,sep="")
-      analysis <- check(url1)$audio_features
-      url2 <- paste("https://api.spotify.com/v1/tracks/?access_token=",access_token,"&ids=",ids,sep="")
-      info <- check(url2)$tracks
+      
+      # Gets track audio-features (50 at a time) - tempo, duration, loudness
+      url3 <- paste("https://api.spotify.com/v1/audio-features/?access_token=",access_token,"&ids=",ids,sep="")
+      analysis <- check(url3)$audio_features
+      
+      # Gets track information (50 at a time) - artist, album, id, popularity, release date
+      url4 <- paste("https://api.spotify.com/v1/tracks/?access_token=",access_token,"&ids=",ids,sep="")
+      info <- check(url4)$tracks
+      
+      # Gets artist ids
       artists <- NULL
       for(l in info$artists){
         artists<- c(artists,paste(l$name,collapse=", "))
@@ -103,30 +142,12 @@ server <- function(input, output) {
     
   })
   
-  playlistdata <- reactive({
-    post <- POST('https://accounts.spotify.com/api/token',
-                 accept_json(), authenticate(client_id, client_secret),
-                 body = list(grant_type = 'client_credentials'),
-                 encode = 'form', httr::config(http_version = 2)) %>% content
-    access_token <- post$access_token
-    
-    playlist_id <- input$playlist_id
-    print(playlist_id)
-    
-    url <- paste("https://api.spotify.com/v1/playlists/",playlist_id,"?access_token=",access_token,sep = "")
-    playlist <- check(url)
-    
-    playlistinfo <- NULL
-    playlistinfo$name <- playlist$name
-    playlistinfo$songs <- playlist$tracks$total
-    playlistinfo$followers<- playlist$followers$total
-    playlistinfo
-  })
-  
   playlistana <- reactive({
     df <- playlist()
     df2 <- playlistdata()
     out <- NULL
+    
+    # Makes list with all necessary information for model
     out$avgpop <- mean(as.numeric(unlist(df["Popularity"])),na.rm = T)
     out$avglen <- mean(as.numeric(unlist(df["Duration"])),na.rm = T)
     out$maxlen <- max(as.numeric(unlist(df["Duration"])),na.rm = T)
@@ -135,10 +156,13 @@ server <- function(input, output) {
     out$followers<- df2$followers
     temp<-c(out$followers,out$songs,out$avgpop,out$avglen,out$maxlen,out$minlen)
     
+    # Gets trained data and runs knn on the playlist
     traindata <- read.csv("playlistsummary.csv",header=T)
     traindata <- traindata[,-1]
     model <- knn.reg(train=traindata,test=temp,y=traindata$numFollowers,k=43)
     
+    # Important: gets the follower prediction, takes the log and divides by the log(3000000) = 6.5, where 3000000 is the max followers of playlists trained on
+    # Logarithm is used so that lowers predictions don't simply return 0, and ensures score is 100 maximum
     min(floor(log(model$pred)*6.5),100)
   })
   
@@ -147,6 +171,7 @@ server <- function(input, output) {
     playlist() 
   })
   
+  # Generates text above the html table
   output$statement <- renderText({
     inf <- playlistdata()
     score <- playlistana()
@@ -162,6 +187,8 @@ server <- function(input, output) {
       s2 <- "There is "
       s3 <-  " track on your playlist."
     }
+    
+    # Creates full string to print to app
     string <- paste("Info for playlist: ",inf$name,"<br/>Your playlist has ", inf$followers, s1,s2,inf$songs, s3,"<br/><font size=\"24\">Playlist score is ",score,"</font>",sep="")
     return(string)
   })
--- Mining.R
+++ Mining.R
@@ -4,6 +4,12 @@ library(dplyr)
 library(mosaic)
 source('config.R')
 
+# check(url)
+# inputs: url to get data from
+# outputs: json data as df
+# 
+# This function's purpose is to circumvent 409 errors (too many requests)
+# Will retry each second for 5 seconds
 check <- function(url){
   attempt <- 0
   res <- NULL
@@ -19,77 +25,111 @@ check <- function(url){
   return(res)
 }
 
+# Gets an access token from the spotify api
 post <- POST('https://accounts.spotify.com/api/token',
              accept_json(), authenticate(client_id, client_secret),
              body = list(grant_type = 'client_credentials'),
              encode = 'form', httr::config(http_version = 2)) %>% content
 access_token <- post$access_token
 
-
+# Creates a series of patterns such that the API query will return playlists starting or ending with each alphabetical character
 alphabet <- c("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
 alphabet <- paste("*",alphabet,"*",sep = "")
+
+# Creates blank dataframe for the data
 data <- data.frame(stringsAsFactors=FALSE)
 colnames(data)<- c("Name","NumSongs","Id","Followers","Tracks")
 
-playlist_50s <- 2
+# Number of each letter to get from playlists in 50s (i.e. playlist_50s <- 2 gets 100 of each letter)
+playlist_50s <- 1
 
+# Loops to search playlist over each alphabetical letter
 for(i in alphabet){
+  # Loops to search 50s of playlists for each alphabetical letter
   for(j in 1:playlist_50s){
     offset <- 50*(j-1)
-    url1 <- paste("https://api.spotify.com/v1/search?type=playlist&limit=50&offset=",offset,"&q=",i,"&access_token=",access_token,sep="")
+    url1 <- paste("https://api.spotify.com/v1/search?type=playlist&limit=1&offset=",offset,"&q=",i,"&access_token=",access_token,sep="")
     playlists <- check(url1)
     print(i)
     playlistsdata <- NULL
+    # Tries to add the data from each playlist to dataframe, some playlists will be outdated, and will not be added
+    # If limit is too small, errors will be returned, but can be ignored
     try(playlistsdata <- data.frame(playlists$playlists$items$name,playlists$playlists$items$tracks$total,playlists$playlists$items$id,"","",stringsAsFactors=FALSE))
-    print(playlistsdata)
     data <- rbind(data,playlistsdata)
   }
 }
+
+# Removes duplicate playlists
 data<-data %>% distinct(playlists.playlists.items.id,.keep_all = T)
-data <- data[-c(grep(TRUE,data[,2] == 0)),]
+
+# Removes empty playlists
+if(0 %in% data[,2]){
+  data <- data[-c(grep(TRUE,data[,2] == 0)),]
+}
+
+# Reorders data, so index doesn't skip numbers
 rownames(data) <- 1:nrow(data)
 
+# Loops through the indices over all playlists
 for(i in 1:length(data[,1])){
   playlist_id <- data[i,3]
   leng <- data[i,2]
   loops <- as.integer(leng/100)
   
-  url <- paste("https://api.spotify.com/v1/playlists/",playlist_id,"?access_token=",access_token,sep = "")
-  followers <- check(url)$followers$total
+  # Gets playlist information (followers)
+  url2 <- paste("https://api.spotify.com/v1/playlists/",playlist_id,"?access_token=",access_token,sep = "")
+  followers <- check(url2)$followers$total
   data[i,4] <- followers
   songs <- NULL
   
+  # Gets all songs in the playlist
   for(j in 0:loops){
     offset <- j*100
-    url <- paste("https://api.spotify.com/v1/playlists/",playlist_id,"/tracks?access_token=",access_token,"&limit=100&offset=",offset,sep = "")
-    df <- check(url)
+    url3 <- paste("https://api.spotify.com/v1/playlists/",playlist_id,"/tracks?access_token=",access_token,"&limit=100&offset=",offset,sep = "")
+    df <- check(url3)
     songs <- c(songs,df$items$track$id)
-    
+  
+  #ignores playlists with no tracks (some only containing videos, audiobooks, etc)  
   }
   if(is.null(songs)){
     next() #thank u
   }
+  
   loops2 <- ceiling(length(songs)/50)
   
+  # Dataframe for track information
   data2 <- data.frame()
+  
+  # Loops over all tracks in playlist
   for(k in 1:loops2){
     ids <- songs[((k-1)*50+1):(k*50)]
     ids <- ids[!is.na(ids)]
     ids <- paste(ids,collapse=",")
-    url1 <- paste("https://api.spotify.com/v1/audio-features/?access_token=",access_token,"&ids=",ids,sep="")
-    analysis <- check(url1)$audio_features
-    url2 <- paste("https://api.spotify.com/v1/tracks/?access_token=",access_token,"&ids=",ids,sep="")
-    info <- check(url2)$tracks
+    
+    # Gets track audio-features (50 at a time) - tempo, duration, loudness
+    url4 <- paste("https://api.spotify.com/v1/audio-features/?access_token=",access_token,"&ids=",ids,sep="")
+    analysis <- check(url4)$audio_features
+    
+    # Gets track information (50 at a time) - artist, album, id, popularity, release date
+    url5 <- paste("https://api.spotify.com/v1/tracks/?access_token=",access_token,"&ids=",ids,sep="")
+    info <- check(url5)$tracks
+    
+    # Gets artist ids
     artists <- NULL
     for(l in info$artists){
       artists<- c(artists,l$id[1])
     }
+    # Adds songs to dataframe
     data2 <- rbind(data2,data.frame(info$id, artists,info$album$release_date, analysis$tempo, analysis$duration_ms, analysis$loudness, info$popularity))
   }
   print(paste("Playlist Number",i,sep = " "))
+  
+  # Some spaghetti code here, R has trouble saving dfs in dfs, so the songs are in a csv string with line separater "slashn"
   foo2 <- capture.output(write.csv(data2, stdout(), row.names=F)) 
   dat2str <- capture.output(cat(foo2,sep="slashn"))
   data[i,5] <- dat2str
+  
+  # Since access tokens last for an hour, refreshes every 100 playlists (safe, could do as many as every 1250 playlists)
   if(i %% 100==0){
     post <- POST('https://accounts.spotify.com/api/token',
                  accept_json(), authenticate(client_id, client_secret),
@@ -99,4 +139,5 @@ for(i in 1:length(data[,1])){
   }
 }
 
-write.csv(data,"datamain.csv")
+#Writes data to csv "datamain.csv" in same directory as 'Mining.R'
+write.csv(data,"datamaintest.csv")
